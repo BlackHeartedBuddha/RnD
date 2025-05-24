2eb8f5725fae9a556de29b2486cc16c391ed696d
Blacklotus89898 <song.yang.chen89898@gmail.com>
2025-05-24 15:34:56 -0400

    navbar and styling + indentation

--- reddit-youtube/app/controllers/subreddits_controller.rb
+++ reddit-youtube/app/controllers/subreddits_controller.rb
@@ -38,38 +38,38 @@ class SubredditsController < ApplicationController
   # PATCH/PUT /subreddits/1 or /subreddits/1.json
   def update
     if current_user && current_user.id == @subreddit.user_id # prevents user to create own button
-    respond_to do |format|
-      if @subreddit.update(subreddit_params)
-        format.html { redirect_to @subreddit, notice: "Subreddit was successfully updated." }
-        format.json { render :show, status: :ok, location: @subreddit }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @subreddit.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @subreddit.update(subreddit_params)
+          format.html { redirect_to @subreddit, notice: "Subreddit was successfully updated." }
+          format.json { render :show, status: :ok, location: @subreddit }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @subreddit.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
   end
 
   # DELETE /subreddits/1 or /subreddits/1.json
   def destroy
     if current_user && current_user.id == @subreddit.user_id # prevents user to create own button
-    @subreddit.destroy!
-  
-    respond_to do |format|
-      format.html { redirect_to subreddits_path, status: :see_other, notice: "Subreddit was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      @subreddit.destroy!
+
+      respond_to do |format|
+        format.html { redirect_to subreddits_path, status: :see_other, notice: "Subreddit was successfully destroyed." }
+        format.json { head :no_content }
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_subreddit
-      @subreddit = Subreddit.friendly.find(params.expect(:id))
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_subreddit
+    @subreddit = Subreddit.friendly.find(params.expect(:id))
+  end
 
-    # Only allow a list of trusted parameters through.
-    def subreddit_params
-      params.expect(subreddit: [ :title, :body, :user_id ])
-    end
+  # Only allow a list of trusted parameters through.
+  def subreddit_params
+    params.expect(subreddit: [ :title, :body, :user_id ])
+  end
 end
